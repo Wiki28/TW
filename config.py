bca534ed33ecd563095a57b9f323850eb4505eb3
Wiki W <99019030+Wiki28@users.noreply.github.com>
2023-02-27 14:24:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 import logging
-
 import os
+
+
+APP_ID = int(os.environ.get("APP_ID", "10037223"))
+API_HASH = os.environ.get("API_HASH", "1f2527315dbd447876973460eb2b9d50")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5902543558:AAFW0iERgSXxsTGD7AJSwTnNJHbopemJrPU")
